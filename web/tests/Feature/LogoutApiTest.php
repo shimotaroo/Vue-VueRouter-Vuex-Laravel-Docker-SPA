352fc230332d191eac55dc595cd35453b204f72e
shimotaroo <shimotaro.0327@gmail.com>
2021-01-21 08:06:09 +0900

    認証APIテストの実装

---
+++ web/tests/Feature/LogoutApiTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class LogoutApiTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function setUp() :void
+    {
+        parent::setUp();
+
+        $this->user = factory(User::class)->create();
+    }
+
+    /**
+     * @test
+     */
+    public function logoutUser ()
+    {
+        $response = $this->actingAs($this->user)
+                            ->json('POST', route('logout'));
+        
+        $response->assertStatus(200);
+        $this->assertGuest();
+    }
+
+}
--- web/tests/Feature/RegisterApiTest.php
+++ web/tests/Feature/RegisterApiTest.php
@@ -2,21 +2,34 @@
 
 namespace Tests\Feature;
 
+use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class RegisterApiTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function testExample()
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    /**
+     * @test
+     */
+    public function registerUser ()
+    {
+        $data = [
+            'name' => 'vuesplash_user',
+            'email' => 'dummy@email.com',
+            'password' => 'test1234',
+            'password_confirmation' => 'test1234'
+        ];
+
+        $response = $this->json('POST', route('register'), $data);
+
+        $user = User::first();
+        $this->assertEquals($data['name'], $user->name);
+
+        $response
+            ->assertStatus(201)
+            ->assertJson(['name' => $user->name]);
     }
 }
